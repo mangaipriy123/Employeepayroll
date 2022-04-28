c96f41cbf76235aaf4bce1e7c54b5ec9bf620173
mangaipriy123 <100937133+mangaipriy123@users.noreply.github.com>
2022-04-28 16:41:31 +0530

    Add files via upload

---
+++ Employeepayroll/App.java
@@ -0,0 +1,13 @@
+package priya.com.Employeepayroll;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
